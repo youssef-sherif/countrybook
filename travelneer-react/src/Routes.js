ac22ad26ed037976e4900209346c16d752b02735
Youssef Sherif <youssefsabry97@gmail.com>
2019-02-10 15:02:42 +0200

    Remove /logout route

--- travelneer-react/src/Routes.js
+++ travelneer-react/src/Routes.js
@@ -5,7 +5,6 @@ import Feed from './views/feed/Feed'
 import Countries from './views/countries/Countries'
 import CountryViewer from './views/countryviewer/CountryViewer'
 import { Route } from 'react-router-dom'
-import LoggedOut from './views/loggedout/LoggedOut'
 
 export default class Routes extends Component {
 
@@ -14,7 +13,6 @@ export default class Routes extends Component {
             <div>
                 <Route path="/" exact component={Home} />
                 <Route path="/feed" exact component={Feed} />
-                <Route path="/logout" exact component={LoggedOut} />
                 <Route path="/countries" exact render={(props) => <Countries {...props} search={false} />} />
                 <Route path="/search_countries" render={(props) => <Countries {...props} search={true} />} />                                
                 <Route path="/countries/:countryId" exact render={(props) => <CountryViewer {...props} compose={false} />} />
