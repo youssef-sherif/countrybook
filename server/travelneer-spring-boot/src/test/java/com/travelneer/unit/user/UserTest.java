b94e0e831e12353796211c5034549d6f00920445
Youssef Sherif <youssefsabry97@gmail.com>
2019-03-07 11:35:58 +0200

    Test that password encrypts and matches correctly

--- server/travelneer-spring-boot/src/test/java/com/travelneer/unit/user/UserTest.java
+++ server/travelneer-spring-boot/src/test/java/com/travelneer/unit/user/UserTest.java
@@ -1,8 +1,13 @@
 package com.travelneer.unit.user;
 
+import com.travelneer.user.Email;
+import com.travelneer.user.Password;
 import com.travelneer.user.User;
+import com.travelneer.user.Username;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,10 +18,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UserTest {
 
     @Test
-    public void whenSettingPassword_ItShouldEncrypt() {
-        User user = new User();
-        String password = "12345678";
-        user.setPassword(password);
-        assertThat(user.getPassword()).isNotEqualTo(password);
+    public void onCreation_PasswordShouldEncrypt_And_PasswordShouldMatchEncrypted() {
+        String passwordValue = "12345678";
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        Password password = new Password(passwordValue);
+        password.encode(passwordEncoder);
+
+
+        User user = new User(new Username("youssef"),
+                new Email("youssef@travelneer.com"),
+                password);
+
+        assertThat(user.getPassword()).isNotEqualTo(passwordValue);
+        assertThat(passwordEncoder.matches(passwordValue, password.getEncoded())).isEqualTo(true);
+
     }
 }
