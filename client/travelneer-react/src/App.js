9cf51cf69c59483f8cf9aa5feb98d8bfe3c8d1da
Youssef Sherif <youssefsabry97@gmail.com>
2019-06-13 11:02:34 +0200

    Fix tests

--- client/travelneer-react/src/App.js
+++ client/travelneer-react/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 import Routes from './Routes'
+import { ConnectedRouter } from 'connected-react-router'
+import { Provider } from 'react-redux'
+import myStore from './storeConfig'
+import myHistory from './history'
+
 
 export default class App extends Component {
 
     render() {
 
         return (
-            <Routes />
+        <Provider store={myStore}>
+            <ConnectedRouter history={myHistory}>
+			    <Routes />
+		    </ConnectedRouter>
+        </Provider>
         )
     }
 }
--- client/travelneer-react/src/Routes.js
+++ client/travelneer-react/src/Routes.js
@@ -4,20 +4,20 @@ import Home from './views/home/Home'
 import Feed from './views/feed/Feed'
 import Countries from './views/countries/Countries'
 import CountryViewer from './views/countryviewer/CountryViewer'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 export default class Routes extends Component {
 
     render() {
         return (
-            <div>
-                <Route path="/" exact component={Home} />
-                <Route path="/feed" exact component={Feed} />
-                <Route path="/countries" exact render={(props) => <Countries {...props} search={false} />} />
-                <Route path="/search_countries" render={(props) => <Countries {...props} search={true} />} />                                
-                <Route path="/countries/:countryId" exact render={(props) => <CountryViewer {...props} compose={false} />} />
-                <Route path="/countries/:countryId/new" exact render={(props) => <CountryViewer {...props} compose={true} />} />                
-            </div>
-        )
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/feed" exact component={Feed} />
+                    <Route path="/countries" exact render={(props) => <Countries {...props} search={false} />} />
+                    <Route path="/search_countries" render={(props) => <Countries {...props} search={true} />} />                                
+                    <Route path="/countries/:countryId" exact render={(props) => <CountryViewer {...props} compose={false} />} />
+                    <Route path="/countries/:countryId/new" exact render={(props) => <CountryViewer {...props} compose={true} />} />                
+                </Switch>            
+                )
     }
 }
