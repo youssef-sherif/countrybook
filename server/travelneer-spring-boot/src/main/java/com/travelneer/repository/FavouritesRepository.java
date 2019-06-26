e078d66bfcff8085d9c3089e79a98b920f3d86d4
Youssef Sherif <youssefsabry97@gmail.com>
2019-06-26 09:57:48 +0200

    Change filename from docker-compose.yaml to docker-compose.yml

--- server/travelneer-spring-boot/src/main/java/com/travelneer/repository/FavouritesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.travelneer.repository;
-
-import com.travelneer.dto.Favourites;
-
-
-public interface FavouritesRepository extends IRepository<Favourites> {
-
-    Boolean isPostFavouriteByUser(Integer postId, Integer userId);
-}
