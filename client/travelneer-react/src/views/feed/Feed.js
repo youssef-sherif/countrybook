623b370699616338764c53406958d002881f9ed9
Youssef Sherif <youssefsabry97@gmail.com>
2019-07-05 14:07:24 +0200

    Add fromFeed prop to PostList component in Feed

--- client/travelneer-react/src/views/feed/Feed.js
+++ client/travelneer-react/src/views/feed/Feed.js
@@ -55,7 +55,7 @@ class Feed extends Component {
                     </div>
                 }              
                 <br/>      
-                <PostList fromCountryViewer={false}/>
+                <PostList fromFeed={true}/>
             </div>)
     }
 
