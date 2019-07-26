64886d0d3e6dbaa427551ea798ae8a75dae925cf
Youssef Sherif <youssefsabry97@gmail.com>
2019-07-26 18:17:42 +0200

    Move Routes.js to routes/Routes.js

--- client/travelneer-react/src/App.js
+++ client/travelneer-react/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Routes from './Routes'
+import Routes from './routes/Routes'
 import { ConnectedRouter } from 'connected-react-router'
 import { Provider } from 'react-redux'
 import myStore from './storeConfig'
